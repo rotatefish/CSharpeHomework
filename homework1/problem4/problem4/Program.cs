490bafd1c499dd786413a579983fb59649289662
rotatefish <lcywhu@163.com>
2019-02-24 00:41:39 +0800

    first commit

---
+++ homework1/problem4/problem4/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace problem4
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Input two numbers:");
+			double a = double.Parse(Console.ReadLine());
+			double b = double.Parse(Console.ReadLine());
+			double ans = a * b;
+			Console.WriteLine("The product of two numbers is : " + ans);
+			Console.ReadLine();
+		}
+	}
+}
