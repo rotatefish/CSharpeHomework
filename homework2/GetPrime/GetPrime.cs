40f054e5ef9c8dc3c74978574393db15f29a8d2c
rotatefish <lcywhu@163.com>
2019-03-07 17:03:39 +0800

    homework2

---
+++ homework2/GetPrime/GetPrime.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GetPrime
+{
+	class GetPrime
+	{
+		static void Main(string[] args)
+		{
+			int[] isPrime = new int[110];
+			for (int i = 2; i <= 100; i++) isPrime[i] = 1;
+			for (int i = 2; i <= 100; i++)
+			{
+				if (isPrime[i] == 0) continue;
+				for (int j = 2; i * j <= 100; j++)
+				{
+					isPrime[i * j] = 0;
+				}
+			}
+			Console.WriteLine("100以内的所有素数:");
+			for (int i = 2; i <= 100; i++)
+			{
+				if (isPrime[i] == 1)
+				{
+					Console.Write(i + " ");
+				}
+			}
+			Console.Read();
+		}
+	}
+}
---
+++ homework2/PrimeNumber/PrimeNumber.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrimeNumber
+{
+	class PrimeNumber
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("输入一个数字:");
+			int num = int.Parse(Console.ReadLine());
+			int tmp = num;
+			int k = 2;
+			Console.WriteLine("素数因子: ");
+			while (tmp > 0)
+			{
+				if (tmp < k) break;
+				if (tmp % k == 0)
+				{
+					Console.Write(k + " ");
+					while (tmp > 0 && tmp % k == 0) tmp /= k;
+				}
+				else
+				{
+					++k;
+				}
+			}
+			Console.ReadLine();
+		}
+	}
+}
