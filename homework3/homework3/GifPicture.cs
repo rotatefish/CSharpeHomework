b95c04daa0a1b4e7f337fa7bf942113744728d5a
rotatefish <lcywhu@163.com>
2019-03-15 20:46:49 +0800

    first commit

---
+++ homework3/homework3/GifPicture.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace homework3 {
+	class GifPicture : Picture {
+		public override void display() {
+			//diplay a gif picture
+			Console.WriteLine("This is a gif picture!");
+		}
+	}
+}
---
+++ homework3/homework3/client.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace homework3 {
+	class Client {
+		static void Main(string[] args) {
+			try {
+				Picture pic;
+				PictureReader reader;
+				reader = new JpgPictureReader();
+				pic = reader.readPicture();
+				pic.display();
+			}
+			catch (Exception e) {
+				Console.WriteLine(e.Message);
+			}
+		}
+	}
+}
